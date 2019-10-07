ad6941ea007f58dcb8270fbd931afaf5485435db
Conor Shields <cshields236@gmail.com>
2019-10-07 14:53:58 +0100

    Tip calculation added

--- app/src/main/java/com/example/loyaltycardapp/MainActivity.java
+++ app/src/main/java/com/example/loyaltycardapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.loyaltycardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate (View view){
+        EditText bill = findViewById(R.id.billTxt);
+        EditText tipPercent = findViewById(R.id.tipPercentTxt);
+
+        String s1 = bill.getText().toString();
+        String s2 = tipPercent.getText().toString();
+        int tip = Integer.parseInt(s1) * Integer.parseInt(s2) / 100;
+        float total = Float.parseFloat(s1) + Float.parseFloat(String.valueOf(tip));
+
+
+
+        TextView r = findViewById(R.id.totalBillView);
+
+
+        r.setText(String.valueOf(total));
+
     }
 }
